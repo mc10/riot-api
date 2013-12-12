8fa6388521f256cf534ebabfc927d11c75615ea3
Kevin Ji <kevin.ji@outlook.com>
2013-12-11 18:18:33 -0800

    Create Python project with .gitignore

---
+++ src/python/riot_api.py
@@ -0,0 +1,5 @@
+import riot_region
+
+class Api:
+    def __init__(self, api_key, region="NA"):
+        self.api_key = api_key
---
+++ src/python/riot_region.py
@@ -0,0 +1,6 @@
+class Region:
+    REGIONS = ['BR', 'EUNE', 'EUW', 'NA', 'TR'];
+
+    @classmethod
+    def is_region(cls, region):
+        return region in cls.REGIONS
