7d53154c68c088d623aa76f93c1c946a089434f6
Kevin Ji <kevin.ji@outlook.com>
2014-07-22 18:39:42 -0700

    ApiVersion: Revert conversion, but adjust naming
    Namespaces do not support the use of global variables within them
    (i.e. no CommonJS-style exporting). Silly PHP.

---
+++ src/riot-api-version.php
@@ -0,0 +1,12 @@
+<?php
+	namespace RiotApi;
+
+	final class ApiVersion {
+		public static $champion = '1.2';
+		public static $game = '1.3';
+		public static $league = '2.4';
+		public static $staticData = '1.2';
+		public static $stats = '1.3';
+		public static $summoner = '1.4';
+		public static $team = '2.3';
+	}
