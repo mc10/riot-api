a7eadd93d75c7465c6c9e3298e33d10f4e39b206
Kevin Ji <kevin.ji@outlook.com>
2014-07-22 17:49:50 -0700

    Create API version static class

---
+++ src/riot-api-version.php
@@ -0,0 +1,12 @@
+<?php
+	namespace RiotApi;
+
+	final class ApiVersion {
+		const CHAMPION = '1.2';
+		const GAME = '1.3';
+		const LEAGUE = '2.4';
+		const STATIC_DATA = '1.2';
+		const STATS = '1.3';
+		const SUMMONER = '1.4';
+		const TEAM = '2.3';
+	}
