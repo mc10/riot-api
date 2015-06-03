aba8c026e0e338a7c3722d0138ea570c314a9b36
Kevin Ji <kevin.ji@outlook.com>
2015-06-03 02:35:00 -0700

    JsonError: Simplify json_last_error_msg polyfill

--- src/RiotApi/JsonError.php
+++ src/RiotApi/JsonError.php
@@ -1,31 +1,16 @@
 <?php
 	if (!function_exists('json_last_error_msg')) {
 		function json_last_error_msg() {
-			switch (json_last_error()) {
-				case JSON_ERROR_DEPTH:
-					$error = 'Maximum stack depth exceeded';
-					break;
+			static $ERRORS = array(
+				JSON_ERROR_NONE => 'No error',
+				JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+				JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
+				JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+				JSON_ERROR_SYNTAX => 'Syntax error',
+				JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+			);
 
-				case JSON_ERROR_STATE_MISMATCH:
-					$error = 'Underflow or the modes mismatch';
-					break;
-
-				case JSON_ERROR_CTRL_CHAR:
-					$error = 'Unexpected control character found';
-					break;
-
-				case JSON_ERROR_SYNTAX:
-					$error = 'Syntax error, malformed JSON';
-					break;
-
-				case JSON_ERROR_UTF8:
-					$error = 'Malformed UTF-8 characters, possibly incorrectly encoded';
-					break;
-
-				default:
-					return;
-			}
-
-			return $error;
+			$error = json_last_error();
+			return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
 		}
 	}
